1291fdbe8544302b72ec3252dceed38ba270b5f5
trantuyen1411 <trantuyen1411@gmail.com>
2023-05-03 21:16:20 -0700

    wed 5/3

--- app.js
+++ app.js
@@ -1,8 +1,78 @@
 const express = require("express");
+const morgan = require ("morgan")
+const postBank = require("./postBank")
+const path = require('path');
+
 const app = express();
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get('/', (_req, res) => {
+  const posts = postBank.list();
+  const HTML =  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+res.sendFile(path.join(__dirname, 'public', 'style.css'));
+ res.send(HTML);
+ 
+});
+
+// say that a client GET requests the path /posts/7
+app.get( '/posts/:id', (req, res) => {
+  const id = req.params.id  
+  const post = postBank.find(id);
+  const HTML = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      
+        <div class='news-item'>
+          <p>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.content} 
+          </small>
+    </div>
+  </body>
+</html>`
+  res.send(HTML);
+});
+
+
+app.get('/logo.png', (_req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'logo.png'));
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = 1337;
 
 app.listen(PORT, () => {
